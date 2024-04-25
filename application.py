f4e92a88f25f5ab467def3a3a70e603145fa2d0b
Preethi3101 <67048706+Preethi3101@users.noreply.github.com>
2024-04-25 12:02:29 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
