517e85ce3be4c4e9beeb35550765490814231154
Preethi3101 <67048706+Preethi3101@users.noreply.github.com>
2024-05-15 14:19:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -124,9 +124,8 @@ def main():
                 text_chunks = get_text_chunks(combined_text)
                 get_vectorstore(text_chunks)
                 st.success("Done")
-
     # Main content area for displaying chat messages
-    st.title("OPENAI CHATBOT")
+    st.title("Chat with PDF files")
     st.write("Welcome to the chat!")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
@@ -134,13 +133,34 @@ def main():
     # Placeholder for chat messages
 
     if "messages" not in st.session_state.keys():
-        st.session_state.messages = [{"role": "assistant", "content": "upload some pdfs/ppts and ask me a question"}]
+        st.session_state.messages = [
+            {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+      # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
             message = {"role": "assistant", "content": full_response}
             st.session_state.messages.append(message)
 
 
 if __name__ == "__main__":
     main()
+
